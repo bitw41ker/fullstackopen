c1a21c571b4b06c9df4d88a00264e21540bfecd8
Bitw41ker <bitw41ker@gmail.com>
2022-08-18 19:46:04 +0300

    2.15: puhelinluettelo step7

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -31,9 +31,12 @@ const App = () => {
       number: newNumber
     }
 
-    setPersons(persons.concat(newPerson));
     setNewName('');
     setNewNumber('');
+
+    axios
+      .post('http://localhost:3001/persons', newPerson)
+      .then(response => setPersons(persons.concat(response.data)));
   }
 
   return (
