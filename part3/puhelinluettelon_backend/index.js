ab9fc7a63b78925242737eb75b6a1056ada8959c
Bitw41ker <bitw41ker@gmail.com>
2022-09-17 20:29:17 +0300

    3.7: puhelinluettelon backend step7

--- part3/puhelinluettelon_backend/index.js
+++ part3/puhelinluettelon_backend/index.js
@@ -1,7 +1,8 @@
-const { request, application, response } = require('express');
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 app.use(express.json());
+app.use(morgan('tiny'))
 
 const PORT = 3001;
 let persons = [
