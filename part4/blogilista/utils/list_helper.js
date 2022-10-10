51009fe372287c771e7ef7e73b2ee696686a4843
Bitw41ker <bitw41ker@gmail.com>
2022-10-10 20:57:02 +0300

    4.5*: apufunktioita ja yksikkötestejä, step3

--- part4/blogilista/utils/list_helper.js
+++ part4/blogilista/utils/list_helper.js
@@ -6,7 +6,18 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  let mostLikesBlog = blogs[0];
+  blogs.forEach((blog) => {
+    if (blog.likes > mostLikesBlog.likes) {
+      mostLikesBlog = blog;
+    }
+  });
+  return mostLikesBlog;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
