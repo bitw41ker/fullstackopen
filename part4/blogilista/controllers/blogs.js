6b41d60bc1a606b4d320fdf9fb6fc02a470bcfcb
Bitw41ker <bitw41ker@gmail.com>
2022-10-17 22:37:51 +0300

    4.10: blogilistan testit, step3

--- part4/blogilista/controllers/blogs.js
+++ part4/blogilista/controllers/blogs.js
@@ -12,12 +12,15 @@ blogsRouter.get('/', async (request, response, next) => {
   }
 });
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body);
+blogsRouter.post('/', async (request, response, next) => {
+  try {
+    const blog = new Blog(request.body);
+    const result = await blog.save();
 
-  blog.save().then((result) => {
     response.status(201).json(result);
-  });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = blogsRouter;
--- part4/blogilista/tests/blog_api.test.js
+++ part4/blogilista/tests/blog_api.test.js
@@ -13,17 +13,46 @@ beforeEach(async () => {
   await Promise.all(promises);
 });
 
-test('all blogs are returned', async () => {
-  const res = await api.get('/api/blogs');
+describe('GET /blogs', () => {
+  test('all blogs are returned', async () => {
+    const res = await api.get('/api/blogs').set('Accept', 'application/json');
 
-  expect(res.body).toHaveLength(blogs.length);
+    expect(res.headers['content-type']).toMatch(/json/);
+    expect(res.status).toEqual(200);
+    expect(res.body).toHaveLength(blogs.length);
+  });
+
+  test('blog identifiers field is id and not _id', async () => {
+    const res = await api.get('/api/blogs');
+
+    expect(res.body[0].id).toBeDefined();
+    expect(res.body[0]._id).not.toBeDefined();
+  });
 });
 
-test('blog identifiers field is id and not _id', async () => {
-  const res = await api.get('/api/blogs');
+describe('POST /blogs', () => {
+  test('blog is added', async () => {
+    const res = await api.post('/api/blogs').send({
+      _id: '5a433b3a1b54a676234d17f9',
+      title: 'Statement Considered',
+      author: 'Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 8,
+    });
 
-  expect(res.body[0].id).toBeDefined();
-  expect(res.body[0]._id).not.toBeDefined();
+    expect(res.status).toEqual(201);
+
+    const res2 = await api.get('/api/blogs/');
+
+    expect(res2.body).toHaveLength(blogs.length + 1);
+    expect(res2.body[blogs.length]).toEqual({
+      id: '5a433b3a1b54a676234d17f9',
+      title: 'Statement Considered',
+      author: 'Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 8,
+    });
+  });
 });
 
 afterAll(() => {
