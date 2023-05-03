763ada4db8b1679fdac6492a7410dd8b9d7ddfc1
bitw41ker <bitw41ker@gmail.com>
2023-05-03 19:28:56 +0300

    5.10: blogilistan frontend, step10

--- part5/bloglist-frontend/src/components/Blogs.js
+++ part5/bloglist-frontend/src/components/Blogs.js
@@ -3,9 +3,11 @@ import Blog from './Blog';
 export default function Blogs({ blogs }) {
   return (
     <div>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} />
+        ))}
     </div>
   );
 }
