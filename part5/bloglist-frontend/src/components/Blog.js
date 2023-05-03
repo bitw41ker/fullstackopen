1d32ded935ece952c7f862dd6ce5980705f87e9f
bitw41ker <bitw41ker@gmail.com>
2023-05-03 17:31:48 +0300

    5.7 blogilistan frontend, step7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,43 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState, useEffect } from 'react';
 
-export default Blog
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: 'solid',
+  borderWidth: 1,
+  marginBottom: 5,
+};
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  let viewButtonLabel = 'View';
+  if (showDetails) {
+    viewButtonLabel = 'Hide';
+  }
+
+  return (
+    <div>
+      <div style={blogStyle}>
+        <span>
+          {blog.title} {blog.author}{' '}
+        </span>
+        <button onClick={() => setShowDetails(!showDetails)}>
+          {viewButtonLabel}
+        </button>
+        {showDetails && (
+          <div>
+            <div>{blog.url}</div>
+            <div>
+              <span>{blog.likes} </span>
+              <button>Like</button>
+            </div>
+            <div>{blog.author}</div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
