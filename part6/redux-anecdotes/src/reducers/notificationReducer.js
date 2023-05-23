fc23c9c7b6616e86f3742e6b066969fab6d4c09c
bitw41ker <bitw41ker@gmail.com>
2023-05-24 01:04:53 +0300

    6.12 anekdootit, step10

---
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState: '',
+  reducers: {
+    setNotification: (state, action) => action.payload,
+  },
+});
+
+export const { setNotification } = notificationSlice.actions;
+export default notificationSlice.reducer;
