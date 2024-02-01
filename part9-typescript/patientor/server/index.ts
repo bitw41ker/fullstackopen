e037db6c94758a7a01c12eae1137c55d1c37fc40
bitw41ker <bitw41ker@gmail.com>
2024-02-01 15:49:14 +0200

    9.9: Patientor backend, step2

--- part9-typescript/patientor/server/index.ts
+++ part9-typescript/patientor/server/index.ts
@@ -1,6 +1,17 @@
 import express from 'express';
 
 const app = express();
+
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
+
 app.use(express.json());
 
 const PORT = 3001;
