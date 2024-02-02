99ebd48e290a2a201c19f752b33e48126a1b9258
bitw41ker <bitw41ker@gmail.com>
2024-02-02 13:51:20 +0200

    9.12: Patientor backend, step5

--- part9-typescript/patientor/server/src/services/patients.ts
+++ part9-typescript/patientor/server/src/services/patients.ts
@@ -1,5 +1,6 @@
 import patients from '../../data/patients';
 import { Patient, nonSensitivePatientData } from '../types';
+import { v4 as uuidv4 } from 'uuid';
 
 const getPatients = (): Patient[] => {
   return patients;
@@ -15,7 +16,31 @@ const getNonSensitivePatientData = (): nonSensitivePatientData[] => {
   }));
 };
 
+const addPatient = (
+  name: string,
+  dateOfBirth: string,
+  ssn: string,
+  gender: string,
+  occupation: string
+): Patient => {
+  if (!name || !dateOfBirth || !ssn || !gender || !occupation) {
+    throw new Error('Missing fields');
+  }
+
+  const newPatient = {
+    id: uuidv4(),
+    name,
+    dateOfBirth,
+    ssn,
+    gender,
+    occupation,
+  };
+  patients.push(newPatient);
+  return newPatient;
+};
+
 export default {
   getPatients,
   getNonSensitivePatientData,
+  addPatient,
 };
