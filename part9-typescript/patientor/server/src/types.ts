aa1d3382044a672a0e5d512c813bb8a57c157262
bitw41ker <bitw41ker@gmail.com>
2024-02-13 17:26:35 +0200

    9.22: Patientor, step 3, define Entry type

--- part9-typescript/patientor/server/src/types.ts
+++ part9-typescript/patientor/server/src/types.ts
@@ -3,10 +3,6 @@ export interface Diagnosis {
   name: string;
   latin?: string;
 }
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {}
-
 export interface Patient {
   id: string;
   name: string;
@@ -24,3 +20,47 @@ export enum Gender {
 }
 
 export type nonSensitivePatientData = Omit<Patient, 'ssn' | 'entries'>;
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+type DateRange = {
+  startDate: string;
+  endDate: string;
+};
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: DateRange;
+}
+
+type Discharge = {
+  date: string;
+  criteria: string;
+};
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: Discharge;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
